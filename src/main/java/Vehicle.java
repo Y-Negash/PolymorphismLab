11fc174e51594542e47a8b5fc252c792490f172b
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-02-12 15:05:31 +0000

    first commit

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,15 @@
+public abstract class Vehicle {
+
+   private int numberOfWheels;
+   private int maxOccupancy;
+   private String travelEnviron:
+
+//    CONSTRUCTOR
+
+    public Vehicle(int numberOfWheels, int maxOccupancy, String travelEnviron){
+        this.numberOfWheels = numberOfWheels;
+        this.maxOccupancy = maxOccupancy;
+        this.travelEnviron = travelEnviron;
+
+    }
+}
