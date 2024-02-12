dd4e1f0c95ddd7ccc1c87b607ab680575d40c6fc
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-02-12 15:54:04 +0000

    add all classes and class tests

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,2 +1,6 @@
-public class Bus {
+public class Bus extends Vehicle {
+
+    public Bus(int numberOfWheels, int maxOccupancy, String travelEnviron ){
+        super(numberOfWheels, maxOccupancy,travelEnviron);
+    }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BusTest {
+
+    Bus bus;
+
+    @BeforeEach
+    public void setUp(){
+        bus = new Bus(6,80,"Ground");
+    }
+
+    @Test
+
 }
